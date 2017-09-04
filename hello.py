8de0c49d62d387b3654133668c3a1506fdbe15df
asm <sasha@mishchenko.tk>
2017-09-04 22:17:42 +0300

    thirdd release

--- hello.py
+++ hello.py
@@ -4,4 +4,4 @@ from bottle import route, run, template
 def index(name):
     return template('<b>Hello {{name}}</b>!', name=name)
 
-run(host='127.0.0.1', port=8080)
+run(host='127.0.0.1', port=8282)
