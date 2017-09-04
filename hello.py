93cd10947a9bdc011f64f0224eb62cec8c7c61f4
asm <sasha@mishchenko.tk>
2017-09-04 22:54:17 +0300

    4th release

--- hello.py
+++ hello.py
@@ -2,6 +2,6 @@ from bottle import route, run, template
 
 @route('/hello/<name>')
 def index(name):
-    return template('<b>Hello {{name}}</b>!', name=name)
+    return template('<b>Hi {{name}}</b>!', name=name)
 
 run(host='192.168.1.103', port=8282)
